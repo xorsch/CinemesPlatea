1e26c6b3b3f096417c6eec5ad0b5413351e14b44
DaniMonPeris <danimontes@hotmail.es>
2024-03-19 10:22:58 +0100

    Buttons
    Try to solve buttons swiper

--- web/javascript/main.js
+++ web/javascript/main.js
@@ -1,3 +1,4 @@
+// MODAL
 const btnAbrirModal = document.querySelectorAll(".btn-abrir-modal");
 const btnCerrarModal = document.querySelector("#btn-cerrar-modal");
 const modal = document.querySelector(".modal");
@@ -17,6 +18,20 @@ document.getElementById("scrolltotop_parent").addEventListener("click", function
     window.scrollTo({ top: 0, behavior: "smooth" });
 });
 
+// Buttons swiper
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+
+let active = 0;
+
+next.onclick = function(){
+	active += 1;
+	reloadSlider();
+}
+function reloadSlider(){
+	
+}
+
 // Cards carrusel Swiper
 var swiper = new Swiper('.swiper', {
 	effect:"coverflow",
@@ -32,8 +47,8 @@ var swiper = new Swiper('.swiper', {
 		stretch:0
 	},
 	navigation: {
-	  nextEl: '.swiper-button-next',
-	  prevEl: '.swiper-button-prev'
+	  nextEl: '#next',
+	  prevEl: '#prev'
 	},
 	spaceBetween: 1
 });
