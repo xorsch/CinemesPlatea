9675dde7b2b204bdc06536a41534520b024200cb
Jordi <xorsch@example.com>
2024-03-20 23:50:18 +0100

    Creada (borrador) la finestra modal d'inscripcio

--- web/javascript/main.js
+++ web/javascript/main.js
@@ -13,10 +13,41 @@ if (btnAbrirModal.length > 0) {
     });
 }
 
+// MODAL other 
+const btnAbrirModal2 = document.querySelectorAll(".btn-abrir-modal2");
+const btnCerrarModal2 = document.querySelector("#btn-cerrar-modal2");
+const modal2 = document.querySelector(".modal2");
+
+if ((btnCerrarModal2 && modal) != null) {
+    btnCerrarModal2.addEventListener("click", () => { modal2.close(); });
+}
+
+if (btnAbrirModal2.length > 0) {
+    btnAbrirModal2.forEach((btn) => {
+        btn.addEventListener("click", () => { modal2.showModal(); });
+    });
+}
+
 // Botón flotante scrollToTop
 document.getElementById("scrolltotop_parent").addEventListener("click", function() {
     window.scrollTo({ top: 0, behavior: "smooth" });
 });
+
+// TODO: Caldria que al principi tingués la opacitat mínima
+// xorrades. 
+document.getElementById("scrolltotop_parent").addEventListener("mouseover", mouseOver);
+document.getElementById("scrolltotop_parent").addEventListener("mouseout", mouseOut);
+
+function mouseOver() {
+	// console.log("ratoli fora" );
+	document.getElementById("scrolltotop_parent").style.opacity = 0.8;
+}
+
+function mouseOut() {
+	// console.log("ratoli fora" );
+	document.getElementById("scrolltotop_parent").style.opacity = 0.2;
+}
+
 
 // Cards carrusel Swiper
 var swiper = new Swiper('.swiper', {
